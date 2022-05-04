f7baddf5147fcf7c80bcf5b025f1f1fddb3d3111
Farnaz Zamani Esfahlani <53013445+FarnazZE@users.noreply.github.com>
2022-05-04 13:26:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -358,7 +358,7 @@ def distance_bin(G):
 
 data_file = str(config['conmat'])
 print("Loading connectivity matrix...")
-sc = np.loadtxt(data_file) #load data 
+sc = np.loadtxt(data_file,delimiter=',') #load data 
 a=sc
 abin=sc.copy()
 abin[abin>0]=1
