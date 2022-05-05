d2520d79ddf97d01c1c5c9928054b5ee0706c715
Farnaz Zamani Esfahlani <53013445+FarnazZE@users.noreply.github.com>
2022-05-04 22:09:15 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -x
 set -e
 
 docker build -t filsilva/cxnetwork .
-docker tag filsilva/cxnetwork filsilva/cxnetwork:0.1.0
+docker tag filsilva/cxnetwork filsilva//cxnetwork:0.2.3
 docker push filsilva/cxnetwork
